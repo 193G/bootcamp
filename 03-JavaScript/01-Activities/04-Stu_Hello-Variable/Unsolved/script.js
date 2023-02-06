26de62a6e437d64e869f292e5acb72905363deed
193G <121628432+193G@users.noreply.github.com>
2023-02-06 22:07:44 +0000

    updating new material for week 3 class 1

---
+++ 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -0,0 +1,14 @@
+// Write Your JavaScript Code Here
+var personName
+
+var personName = "harun"
+
+var pets
+
+var pets = "2 pets"
+
+var funFact 
+
+var funFact = "iDontKnowAnyFunFacts"
+
+console.log("my name is " + personName, "and i have " + pets, "and i " + funFact);
---
+++ 03-JavaScript/01-Activities/06-Stu_Primitive-Types/Unsolved/script.js
@@ -0,0 +1,34 @@
+
+// var one is an integer variable
+var one = 64;
+// var two is a string variable
+var two = "656302";
+// var three is a boolean variable 
+var three = false;
+// var four is a decimal variable
+var four = 64.55;
+// var five is a string variable
+var five = "Howdy!";
+// var siz in an undefined variable
+var six;
+
+// Insert comments to explain what each console log below will log to the console
+// re-asigned four to string
+// re-asigned five to boolean
+// re-asigned six to integar
+console.log(typeof one); 
+console.log(typeof two); 
+console.log(typeof three); 
+console.log(typeof four); 
+console.log(typeof five);
+console.log(typeof six);
+
+four = "Hello!";
+five = false;
+six = 23;
+
+// Insert comments to explain what each console log below will log to the console
+
+console.log(typeof four);
+console.log(typeof five);
+console.log(typeof six);
