b72d9e1914c8a97a4dd19305060df474d63d57ca
193G <121628432+193G@users.noreply.github.com>
2023-02-07 21:35:16 +0000

    updating with week 3 day 2

--- 03-JavaScript/01-Activities/12-Stu_Arrays/Unsolved/script.js
+++ 03-JavaScript/01-Activities/12-Stu_Arrays/Unsolved/script.js
@@ -1,1 +1,5 @@
 // WRITE YOUR CODE HERE
+var students = [" ashley", "ynonne", "ibrahim", "harun"];
+
+console.log ("welcome to the class" + students[0]);
+
--- 03-JavaScript/01-Activities/14-Stu_Iteration/Unsolved/script.js
+++ 03-JavaScript/01-Activities/14-Stu_Iteration/Unsolved/script.js
@@ -1,1 +1,7 @@
 //WRITE YOUR CODE BELOW
+
+var students = [" yvonne", " hodan", " ashley"];
+
+for (var i = 0; i < students.length; i++) {
+    console.log("whats up" + students[i] + ",");
+}
